1a795ee44548734caa34e94f2f829eefccb6bf33
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2022-10-03 19:20:34 +0200

    add initial files

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("What is your name?");
+    let mut name = String::new();
+    let greeting = "Nice to meet you";
+    io::stdin().read_line(&mut name)
+        .expect("Didn't Receive Input");
+
+    println!("Hello, {}! {}", name.trim_end(), greeting);
+
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age = "47";
+    let mut age: u32 = age.trim().parse()
+    .expect("Age wan't assigned a number");
+    age = age +1;
+    print!( "I'm {} and I want ${}", age, ONE_MIL);
+}
